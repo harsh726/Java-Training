96242ac2dfc61489b41f3d1a77a57047be55d923
ladharsh726 <ladharsh726@gmail.com>
2025-04-15 12:13:43 -0400

    Completed "Naming Convention"

---
+++ NamingConvention/NamingConvention.java
@@ -0,0 +1,19 @@
+package NamingConvention;
+
+class Demo{ // Class name with one word
+    public void display(){ // Method name with one word
+
+    }
+    public void getId() { // Method name with two words
+
+    }
+}
+public class NamingConvention { // Class name with 2 words
+    public static void main(String[] args) {
+        int id; // Variable with one word
+        String studentName; // Variable with two words
+
+        double PI; // Constant variable with one word
+        String MY_DATA; // Constant variable with two words
+    }
+}
